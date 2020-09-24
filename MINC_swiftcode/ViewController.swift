7d087b78fd499e6a6c09c5be19fc44c11dbc7731
Finlay MacGregor <finlay.n.macgregor@gmail.com>
2020-09-24 09:13:14 +0200

    Initial Commit

---
+++ MINC_swiftcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MINC_swiftcode
+//
+//  Created by Finlay MacGregor on 9/24/20.
+//  Copyright © 2020 Finlay MacGregor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
